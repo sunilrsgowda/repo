aef5be0ba94878ab5a36de95002e3c91971b15df
sunilrsgowda <sunil.kumar@gndsolutions.in>
2023-12-02 09:31:13 +0530

    first commit

---
+++ code1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    print("enter a number");
+    scanf("%d, %d",&a,&b);
+    printf("the value of a and b is :",a,b);
+    return 0;
+
+
+}
---
+++ code2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("good morning");
+    return 0;
+}
